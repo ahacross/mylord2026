639d4e57e3ee2b48e14289d87a50950b3e9bdfb7
이윤기 <ahacross@naver.com>
2026-07-24 01:56:27 +0900

    dialog 관련 수정

--- packages/form/src/dialog/index.ts
+++ packages/form/src/dialog/index.ts
@@ -5,8 +5,5 @@ import { createVfm as createVfmOriginal } from 'vue-final-modal'
 export const createVfm = (): any => createVfmOriginal()
 
 export { default as DialogComponent } from './components/DialogComponent.vue'
-export { default as CmnDialog } from './components/CmnDialog.vue'
-export * from './composables/useDialogHistory'
 export * from './composables/useDialog'
 export { ModalsContainer, useModal, useModalSlot, useVfm } from 'vue-final-modal'
-export { useDialogStore } from './stores/dialog'
